3b6d3803264b1916957fee9568c1d069f68f51e9
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-08-29 20:50:14 -0500

    Avance js inicio sesion

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var btn = document.getElementById("btnInicioSesion");
+  btn.onclick = function() {
+    iniciarSesion()
+  };
+
+});
+
+
+
+async function iniciarSesion () {
+  console.log('martin')
+}
