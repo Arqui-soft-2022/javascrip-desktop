ffed93eedb19f3e6389fc200b81229f396e2f81b
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-08-30 21:39:54 -0500

    Inicio sesion validacion js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,34 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+const API_LOGIN_URL= "https://codeqr-generate.herokuapp.com/api"
 
-  var btn = document.getElementById("btnInicioSesion");
-  btn.onclick = function() {
-    iniciarSesion()
-  };
+const formulario = document.getElementById('formulario')
 
-});
+const usuario = document.getElementById('usuario')
+const password = document.getElementById('password')
 
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault()
+  iniciarSesion()
+})
 
-
-async function iniciarSesion () {
-  console.log('martin')
+async function iniciarSesion() {
+  try {
+    let data = {
+      username: usuario.value,
+      password: password.value
+    }
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+    const response = await fetch(`${API_LOGIN_URL}/auth/login`, options)
+    const dataResponse = await response.json()
+    if (dataResponse.errors && dataResponse.errors.length > 0) {
+      alert(dataResponse.errors[0].msg)
+    }
+  } catch (error) {
+    console.log(error[0])
+  }
 }
